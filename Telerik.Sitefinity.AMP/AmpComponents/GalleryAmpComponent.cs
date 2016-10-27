f0f901246f16cbb933003a5346b5fc53eaec516a
Izabel Gandileva <Izabel.Gandileva@telerik.com>
2016-10-27 10:34:09 +0300

    Added GalleryAmpComponent

---
+++ Telerik.Sitefinity.AMP/AmpComponents/GalleryAmpComponent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Telerik.Sitefinity.Libraries.Model;
+using Telerik.Sitefinity.Model;
+
+namespace Telerik.Sitefinity.AMP.AmpComponents
+{
+	public class GalleryAmpComponent : IAmpComponent
+	{
+		private string ampImgGalleryTemplate = "<div class=\"slide\"><amp-img src=\"{0}\" alt=\"{1}\" height=\"{2}\" width=\"{3}\" layout=\"responsive\"></amp-img></div>";
+
+		private string ampGalleryTemplate = "<amp-carousel layout=\"fill\" type=\"slides\">{0}</amp-carousel>";
+
+		public string Generate(object fieldValue)
+		{
+			var images = ((IEnumerable<IDataItem>)fieldValue).Cast<Image>().ToList();
+
+			if (images != null && images.Any())
+			{
+				var imagesTemplate = new StringBuilder();
+
+				foreach (var image in images)
+				{
+					imagesTemplate.AppendFormat(ampImgGalleryTemplate, image.MediaUrl, image.AlternativeText, image.Width, image.Height);
+				}
+
+				return string.Format(ampGalleryTemplate, imagesTemplate);
+			}
+
+			return null;
+
+		}
+	}
+}
--- Telerik.Sitefinity.AMP/AmpComponents/ImageAmpComponent.cs
+++ Telerik.Sitefinity.AMP/AmpComponents/ImageAmpComponent.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Telerik.Sitefinity.Libraries.Model;
 using Telerik.Sitefinity.Model;
-using Telerik.Sitefinity.Modules.Libraries;
 
 namespace Telerik.Sitefinity.AMP.AmpComponents
 {
@@ -15,11 +11,10 @@ namespace Telerik.Sitefinity.AMP.AmpComponents
 
 		public string Generate(object fieldValue)
 		{
-			var images = ((IEnumerable<IDataItem>)fieldValue).Cast<Image>();
+			var images = ((IEnumerable<IDataItem>)fieldValue).Cast<Image>().ToList();
 
 			if (images != null && images.Any())
 			{
-				//TODO: Handle multiple images (gallery)
 				var image = images.First();
 				return string.Format(ampImgTemplate, image.MediaUrl, image.AlternativeText, image.Height, image.Width);
 			}
